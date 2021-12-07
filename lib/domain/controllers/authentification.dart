fffb3d861aa8262e63beff1304c6ee9e40ca1b54
Nataly Imitola Rincon <natalyimitolarincon@Natys-MacBook-Air.local>
2021-12-06 20:40:22 -0500

    New code

--- lib/domain/controllers/authentification.dart
+++ lib/domain/controllers/authentification.dart
@@ -28,25 +28,10 @@ class AuthController extends GetxController {
     return uid;
   }
 
-  void registrar(String email, password) {
+  void registrar(String email, password) async {
     try {
-      auth.createUserWithEmailAndPassword(email: email, password: password);
-    } catch (e) {
-      Get.snackbar("About User", "User message",
-          backgroundColor: kSecondaryColor,
-          snackPosition: SnackPosition.BOTTOM,
-          titleText: Text(
-            "Ocurrió un error al crear el usuario",
-            style: TextStyle(color: Colors.white),
-          ),
-          messageText:
-              Text(e.toString(), style: TextStyle(color: Colors.white)));
-    }
-  }
-
-  void ingresar(String email, password) {
-    try {
-      auth.signInWithEmailAndPassword(email: email, password: password);
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
     } catch (e) {
       String errMessage;
       switch (e) {
@@ -64,12 +49,49 @@ class AuthController extends GetxController {
       Get.snackbar("About User", "User message",
           backgroundColor: kSecondaryColor,
           snackPosition: SnackPosition.BOTTOM,
-          titleText: Text(
+          titleText: const Text(
+            "Ocurrió un error al crear el usuario",
+            style: TextStyle(color: Colors.white),
+          ),
+          messageText: Text(errMessage, style: TextStyle(color: Colors.white)));
+    }
+  }
+
+  void ingresar(String email, password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      String errMessage;
+      switch (e) {
+        case 'auth/user-disabled':
+          errMessage = 'Este email ha sido deshabilitado.';
+          break;
+        case 'auth/invalid-email':
+          errMessage = 'Correo inválido';
+          break;
+        case 'auth/user-not-found':
+          errMessage = 'Correo erróneo';
+          break;
+        case 'auth/wrong-password':
+          errMessage = 'Contraseña erróneo';
+          break;
+        default:
+          errMessage =
+              'Ha ocurrido un error al momento de registrar su usuario. Intentelo más tarde.';
+          break;
+      }
+      Get.snackbar("About Login", "Login message",
+          backgroundColor: kSecondaryColor,
+          snackPosition: SnackPosition.BOTTOM,
+          titleText: const Text(
             "Ocurrió un error al ingresar el usuario",
             style: TextStyle(color: Colors.white),
           ),
-          messageText:
-              Text(e.toString(), style: TextStyle(color: Colors.white)));
+          messageText: Text(errMessage, style: TextStyle(color: Colors.white)));
+    }
+
+    void logout() async {
+      await auth.signOut();
     }
   }
 }
